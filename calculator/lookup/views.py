888bdc4008b5b08b1648ec134d94c3953f360070
Pronob <pronobmozumder.info@gmail.com>
2020-05-14 14:47:40 +0600

    first commit

---
+++ calculator/lookup/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request,'home.html',{})
+
+def result(request):
+    val1 = int(request.GET['num1'])
+    val2 = int(request.GET['num2'])
+    res = val1 + val2
+    return render(request,'result.html',{'result':res})
