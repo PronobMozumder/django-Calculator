a7d4e9f9f4bb7c6ca7b45d761f15969cd1527cf6
Pronob <pronobmozumder.info@gmail.com>
2020-05-14 15:13:17 +0600

    2nd commit

--- calculator/lookup/views.py
+++ calculator/lookup/views.py
@@ -8,4 +8,4 @@ def result(request):
     val1 = int(request.GET['num1'])
     val2 = int(request.GET['num2'])
     res = val1 + val2
-    return render(request,'result.html',{'result':res})
+    return render(request,"result.html",{'result':res})
